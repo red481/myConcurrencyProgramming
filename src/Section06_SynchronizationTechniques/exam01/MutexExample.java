9ea600abce7342602371725ee70d091f99a4afed
Dong Soo <rsy48@naver.com>
2024-03-22 23:42:46 +0900

    3/22 상호배제

---
+++ src/Section06_SynchronizationTechniques/exam01/MutexExample.java
@@ -0,0 +1,24 @@
+package Section06_SynchronizationTechniques.exam01;
+
+public class MutexExample {
+    public static void main(String[] args) throws InterruptedException {
+
+        SharedData sharedData = new SharedData(new Mutex());
+
+        Thread thread1 = new Thread(() -> {
+            sharedData.sum();
+        });
+
+        Thread thread2 = new Thread(() -> {
+            sharedData.sum();
+        });
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        System.out.println("SharedData value result: " + sharedData.getSum());
+    }
+}
