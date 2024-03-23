abc7d2ea915ad11791ea98bf403ddc6b1f478243
Dong Soo <rsy48@naver.com>
2024-03-23 16:15:58 +0900

    3/23 모니터 락 학습

---
+++ src/Section06_SynchronizationTechniques/exam03/MutualExclusionExample.java
@@ -0,0 +1,37 @@
+package Section06_SynchronizationTechniques.exam03;
+
+public class MutualExclusionExample {
+
+    private int counter = 0;
+
+    public void increment(){
+        counter++;
+        System.out.println("스레드 : " + Thread.currentThread().getName() + " 카운터 값 : " + counter);
+    }
+
+    public static void main(String[] args){
+        MutualExclusionExample example = new MutualExclusionExample();
+
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 50000; i++) {
+                example.increment();
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < 50000; i++) {
+                example.increment();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
